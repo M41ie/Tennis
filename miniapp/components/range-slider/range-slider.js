aff2a9b6834f0a432d8ae2335245f8f63f4902df
M41ie <maliesky@hotmail.com>
2025-06-07 05:37:11 +0800

    feat(miniapp): add reusable range slider

---
+++ miniapp/components/range-slider/range-slider.js
@@ -0,0 +1,32 @@
+Component({
+  properties: {
+    min: { type: Number, value: 0 },
+    max: { type: Number, value: 100 },
+    step: { type: Number, value: 1 },
+    value: { type: Array, value: [] }
+  },
+  data: {
+    innerMin: 0,
+    innerMax: 0
+  },
+  lifetimes: {
+    attached() {
+      const val = this.properties.value;
+      const min = val.length ? val[0] : this.properties.min;
+      const max = val.length ? val[1] : this.properties.max;
+      this.setData({ innerMin: min, innerMax: max });
+    }
+  },
+  methods: {
+    onChange(e) {
+      const [min, max] = e.detail.value;
+      this.setData({ innerMin: min, innerMax: max });
+      this.triggerEvent('change', { value: [min, max] });
+    },
+    onChanging(e) {
+      const [min, max] = e.detail.value;
+      this.setData({ innerMin: min, innerMax: max });
+      this.triggerEvent('changing', { value: [min, max] });
+    }
+  }
+});
