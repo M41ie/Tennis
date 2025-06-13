add5004e2900f25ba599050013e932ebbf6f2e0b
M41ie <maliesky@hotmail.com>
2025-06-13 21:15:16 +0800

    Move sys admin set leader action

--- miniapp/pages/manage/manage.js
+++ miniapp/pages/manage/manage.js
@@ -5,6 +5,7 @@ Page({
     pending: [],
     members: [],
     isAdmin: false,
+    isSysAdmin: false,
     userId: '',
     clubName: '',
     clubSlogan: '',
@@ -22,8 +23,26 @@ Page({
       wx.setStorageSync('club_id', options.cid);
     }
     this.setData({ userId: wx.getStorageSync('user_id') });
-    this.fetchClub();
-    this.fetchPlayers();
+    this.checkSysAdmin();
+  },
+  checkSysAdmin() {
+    const uid = this.data.userId;
+    const that = this;
+    if (!uid) {
+      that.fetchClub();
+      that.fetchPlayers();
+      return;
+    }
+    wx.request({
+      url: `${BASE_URL}/users/${uid}`,
+      success(res) {
+        that.setData({ isSysAdmin: !!res.data.sys_admin });
+      },
+      complete() {
+        that.fetchClub();
+        that.fetchPlayers();
+      }
+    });
   },
   loadPendingNames(list) {
     const that = this;
@@ -141,7 +160,8 @@ Page({
         const info = res.data;
         const isAdmin =
           info.leader_id === that.data.userId ||
-          (info.admin_ids && info.admin_ids.includes(that.data.userId));
+          (info.admin_ids && info.admin_ids.includes(that.data.userId)) ||
+          that.data.isSysAdmin;
         let role = 'member';
         if (info.leader_id === that.data.userId) role = 'leader';
         else if (info.admin_ids && info.admin_ids.includes(that.data.userId))
