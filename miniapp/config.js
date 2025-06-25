0f1056ba11d6522c1bc15bf0f7fd280120f2eb56
M41ie <maliesky@hotmail.com>
2025-06-25 20:13:39 +0800

    Update config.js

--- miniapp/config.js
+++ miniapp/config.js
@@ -10,8 +10,8 @@ if (typeof wx !== 'undefined' && wx.getAccountInfoSync) {
 
 const CONFIG = {
   develop: { BASE_URL: 'http://119.45.169.39:8002' },
-  trial:   { BASE_URL: 'https://api.example.com:8001' },
-  release: { BASE_URL: 'https://api.example.com:8000' }
+  trial:   { BASE_URL: 'https://api.tennisrating.top:8001' },
+  release: { BASE_URL: 'https://api.tennisrating.top:8000' }
 };
 
 module.exports = CONFIG[env] || CONFIG.develop;
