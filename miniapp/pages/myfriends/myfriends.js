3bc89513cf292ec79f8ac3eb7669a8761411794c
M41ie <maliesky@hotmail.com>
2025-07-16 16:49:21 +0800

    Improve friends page layout and add summary

--- miniapp/pages/myfriends/myfriends.js
+++ miniapp/pages/myfriends/myfriends.js
@@ -2,13 +2,16 @@ const friendService = require('../../services/friend');
 const store = require('../../store/store');
 const { hideKeyboard } = require('../../utils/hideKeyboard');
 const { t } = require('../../utils/locales');
+const IMAGES = require('../../assets/base64.js');
 
 Page({
   data: {
     t,
     list: [],
     isLoading: true,
-    isError: false
+    isError: false,
+    totalFriends: 0,
+    placeholderAvatar: IMAGES.DEFAULT_AVATAR
   },
   hideKeyboard,
   onShow() {
@@ -17,7 +20,18 @@ Page({
     this.setData({ isLoading: true, isError: false });
     friendService.getFriends(uid)
       .then(res => {
-        this.setData({ list: res || [], isLoading: false });
+        const list = (res || []).map(item => {
+          if (!item.matches_against && item.weight !== undefined) {
+            const winRate = item.weight ? ((item.wins || 0) / item.weight * 100).toFixed(1) : 0;
+            item.matches_against = { count: item.weight, win_rate: winRate };
+          }
+          if (!item.matches_partnered && item.partner_games !== undefined) {
+            const winRate = item.partner_games ? ((item.partner_wins || 0) / item.partner_games * 100).toFixed(1) : 0;
+            item.matches_partnered = { count: item.partner_games, win_rate: winRate };
+          }
+          return item;
+        });
+        this.setData({ list, isLoading: false, totalFriends: list.length });
       })
       .catch(() => {
         this.setData({ isError: true, isLoading: false });
